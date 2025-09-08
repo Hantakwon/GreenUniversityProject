4250a459910bbc64417138f7663453adee3a5473
hanto0916 <hanto0916@gmail.com>
2025-09-08 10:55:52 +0900

    edit college

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import dto.DepartmentDTO;
 import util.DBHelper;
+import util.Sql;
 
 /*
  * 날짜 : 2025-09-08
@@ -33,7 +35,67 @@ public class DepartmentDAO extends DBHelper {
 	}
 	
 	public List<DepartmentDTO> selectAll() {
-		return null;
+		List<DepartmentDTO> dtoList = new ArrayList<DepartmentDTO>();
+
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(Sql.SELECT_DEPARTMENT_ALL);
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				DepartmentDTO dto = new DepartmentDTO();
+				dto.setDeptId(rs.getInt(1));
+				dto.setColId(rs.getInt(2));
+				dto.setDeptNo(rs.getString(3));
+				dto.setNameKor(rs.getString(4));
+				dto.setNameEng(rs.getString(5));
+				dto.setFoundedDate(rs.getDate(6));
+				dto.setTel(rs.getString(7));
+				dto.setOffice(rs.getString(8));
+				
+				dtoList.add(dto);
+			}
+			closeAll();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+		return dtoList;
+	}
+	
+	public List<DepartmentDTO> selectAllByCollege(int col_id) {
+		List<DepartmentDTO> dtoList = new ArrayList<DepartmentDTO>();
+		
+		StringBuilder sql = new StringBuilder(Sql.SELECT_DEPARTMENT_ALL);
+
+		sql.append(Sql.SEARCH_WHERE_COLLEGE);
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(sql.toString());
+			psmt.setInt(1, col_id);
+
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				DepartmentDTO dto = new DepartmentDTO();
+				dto.setDeptId(rs.getInt(1));
+				dto.setColId(rs.getInt(2));
+				dto.setDeptNo(rs.getString(3));
+				dto.setNameKor(rs.getString(4));
+				dto.setNameEng(rs.getString(5));
+				dto.setFoundedDate(rs.getDate(6));
+				dto.setTel(rs.getString(7));
+				dto.setOffice(rs.getString(8));
+				
+				dtoList.add(dto);
+			}
+			closeAll();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+		return dtoList;
 	}
 	
 	public void update(DepartmentDTO dto) {
